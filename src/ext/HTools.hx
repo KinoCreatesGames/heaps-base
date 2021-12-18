b48b69e7ef3c1d1bf35e1e38a88e533530de686c
kinor <kinoarose@gmail.com>
2021-12-18 17:31:48 -0500

    Update the pixels in the engine to check for collisions

--- src/ext/HTools.hx
+++ src/ext/HTools.hx
@@ -1,5 +1,7 @@
 package ext;
 
+import hxd.Pixels;
+import h3d.Vector;
 import dn.heaps.Controller.ControllerAccess;
 import h2d.Text.Align;
 
@@ -42,5 +44,47 @@ class HTools {
   public static inline function isAnyKeyDown(ct:ControllerAccess,
       keys:Array<Int>) {
     return keys.exists((key) -> ct.isKeyboardDown(key));
+  }
+
+  /**
+   * Returns the x, y coordinate 
+   * in integer x, y texture space  
+   * @param color 
+   */
+  public static function findPixel(color:Int, collisionPixels:Pixels,
+      collisionMap:h3d.mat.Texture) {
+    var vec = Vector.fromColor(color);
+    vec.a = 1.;
+    for (x in 0...collisionPixels.width) {
+      for (y in 0...collisionMap.height) {
+        var colMapColor = (collisionPixels.getPixelF(x, y));
+        if (vec.equals(colMapColor)) {
+          return new Vector(x, y);
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Takes the world position in floating point
+   * compares to the pixel coordinate 
+   * @param x 
+   * @param y 
+   */
+  public static function isPixelCollide(x:Float, y:Float, color:Int,
+      collisionPixels:Pixels, tex:h3d.mat.Texture) {
+    var width = tex.width;
+    var height = tex.height;
+    // var x = (mode.worldPos.x % 1.);
+    // var y = (mode.worldPos.y % 1.);
+    var pX = Std.int((x * width));
+    var pY = Std.int((y * height));
+    var colMapColor = (collisionPixels.getPixelF(pX, pY));
+    var vec = Vector.fromColor(color);
+    // Note that the alpha channel coming from the pixels is 1
+    // Ends up being 0 from the vector from color we have to account for that;
+    vec.a = 1.;
+    return vec.equals(colMapColor);
   }
 }
